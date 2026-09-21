0780fb552eff0f7446408d9d6e7a20980e6acb5e
Vincent Rodley <vindognz@gmail.com>
2026-09-21 21:25:54 +1200

    added copy game code button

--- ai.py
+++ ai.py
@@ -218,7 +218,7 @@ if __name__ == "__main__":
 	import subprocess
 
 	board = OthelloBoard()
-	ai = OthelloAI(depth=5)
+	ai = OthelloAI(depth=6)
 	times = []
 
 	with open("benchmark.log", "w") as log:
